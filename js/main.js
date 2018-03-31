63142878ef08f6635ae46abaf9764c6d2d3cee4b
abdoutelb <abdoutelb.fci@gmail.com>
2018-03-31 17:55:31 +0200

    Solve code review comments

--- js/main.js
+++ js/main.js
@@ -141,6 +141,8 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
+  image.alt = restaurant.alt;
+  
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
