ed5bdbfd80f4adee23a57293ce8b0b6c36e4a3f2
abdoutelb <abdoutelb.fci@gmail.com>
2018-05-05 17:21:07 +0200

    Cache the restaurant pages

--- sw.js
+++ sw.js
@@ -1,6 +1,8 @@
 var dataCacheName = 'restaurant-v1';
 var cacheName = 'restaurant-app';
 
+console.log("inside.");
+
 // items to be caches
 var cachedItems = [
     'index.html',
@@ -13,43 +15,47 @@ var cachedItems = [
 
 // when sw install
 self.addEventListener('install', function (e) {
-    e.waitUntil(
-        caches.open(cacheName).then(function (cache) {
-            return cache.addAll(cachedItems);
-        })
-    );
+    e.waitUntil(caches.open(cacheName).then(function (cache) {
+        return cache.addAll(cachedItems);
+    }));
 });
 
 // when sw activate
 self.addEventListener('activate', function (e) {
-    e.waitUntil(
-        caches.keys().then(function (keyList) {
-            return Promise.all(keyList.map(function (key) {
-                if (key !== cacheName && key !== dataCacheName) {
-                    return caches.delete(key);
-                }
-            }));
-        })
-    );
-    return self.clients.claim();
+    e.waitUntil(caches.keys().then(function (keyList) {
+        return Promise.all(keyList.map(function (key) {
+            if (key !== cacheName && key !== dataCacheName) {
+                return caches.delete(key);
+            }
+        }));
+    }));
+    return self
+        .clients
+        .claim();
 });
 
 // fetch the cache items
 self.addEventListener('fetch', function (e) {
-    if (e.pathname === '/') {
-        e.respondWith(
-            caches.open(dataCacheName).then(function (cache) {
-                return fetch(e.request).then(function (response) {
-                    cache.put(e.request.url, response.clone());
-                    return response;
-                });
-            })
-        );
-    } else {
-        e.respondWith(
-            caches.match(e.request).then(function (response) {
-                return response || fetch(e.request);
-            })
-        );
+
+    const url = new URL(e.request.url);
+
+    if (url.pathname.startsWith('/restaurant.html')) {
+        e.respondWith(caches.match('restaurant.html').then(response => response || fetch(e.request)));
+        return;
     }
+
+    if (e.pathname === '/') {
+        e.respondWith(caches.open(dataCacheName).then(function (cache) {
+            return fetch(e.request).then(function (response) {
+                cache.put(e.request.url, response.clone());
+                return response;
+            });
+        }));
+    } else {
+        e.respondWith(caches.match(e.request).then(function (response) {
+            return response || fetch(e.request);
+        }));
+    }
+
+    
 });
