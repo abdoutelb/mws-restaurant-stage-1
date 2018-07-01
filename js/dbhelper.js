7e164e5146c586a6cc5e67e8a802de3f954d5dde
Abd Elrahman Telb <abdoutelb.fci@gmail.com>
2018-07-01 02:19:09 +0200

    Update dbhelper
    test heroku app

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,7 +9,8 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     const port = 1337 // Change this to your server port
-    return `http://localhost:${port}/restaurants`;
+    // return `http://localhost:${port}/restaurants`; 
+    return `https://limitless-harbor-91860.herokuapp.com/restaurants`; // test heroku app
   }
 
   /**
