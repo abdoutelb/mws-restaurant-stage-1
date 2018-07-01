e4a5e34cf454cfe848b68585b87fb9001b6e5db2
abdoutelb <abdoutelb.fci@gmail.com>
2018-07-01 16:23:04 +0200

    update sw path

--- js/main.js
+++ js/main.js
@@ -184,7 +184,7 @@ addMarkersToMap = (restaurants = self.restaurants) => {
 // Register Service Worker
 if (navigator.serviceWorker) {
   window.addEventListener('load', function () {
-  navigator.serviceWorker.register('/sw.js')
+  navigator.serviceWorker.register('/restaurants/sw.js')
   .then(() => console.log('Service Worker works !!'));
   });
 }
